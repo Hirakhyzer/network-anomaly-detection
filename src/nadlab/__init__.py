4da3d990ed32e769c2559a82f3bcced38aace85b
Hira Khyzer <55583242+Hirakhyzer@users.noreply.github.com>
2026-07-04 01:03:05 +0800

    Initialize research package

---
+++ src/nadlab/__init__.py
@@ -0,0 +1,5 @@
+"""Network Anomaly Detection Lab research package."""
+
+from .data import FEATURE_COLUMNS, generate_synthetic_telemetry
+
+__all__ = ["FEATURE_COLUMNS", "generate_synthetic_telemetry"]
